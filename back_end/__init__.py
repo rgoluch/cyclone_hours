b4d14979cbb64335af47bcefd7c990d16d75c241
rgoluch <rgoluch@iastate.edu>
2019-09-25 17:10:44 -0500

    adding more for backend

--- back_end/__init__.py
+++ back_end/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
 
 back_end = Flask(__name__)
+back_end.config.from_object(Config)
+db = SQLAlchemy(back_end)
+migrate = Migrate(back_end, db)
+login = LoginManager(back_end)
+login.login_view = 'login'
 
 from back_end import routes
---
+++ back_end/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from back_end import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+class TA(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
